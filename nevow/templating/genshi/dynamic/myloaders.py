f97f3d25a4e8c0074b10c67e493a61e573b30dd1
oubiwann <>
2008-03-27 07:02:43 +0000

    Bug fixes and simple file support.

--- nevow/templating/genshi/dynamic/myloaders.py
+++ nevow/templating/genshi/dynamic/myloaders.py
@@ -3,6 +3,7 @@ from inspect import getmembers
 from cStringIO import StringIO
 
 from nevow import flat
+from nevow import rend
 from nevow import inevow
 from nevow.loaders import xmlstr
 from nevow.loaders import xmlfile
@@ -12,22 +13,39 @@ from genshi.template import TemplateLoader
 
 rendPageAttrs = [x[0] for x in getmembers(rend.Page)]
 
-def getObjectData(obj)
+def getObjectData(obj):
+    data = {}
     for attr, val in getmembers(obj):
+        if not isinstance(attr, str):
+            print "Member name is not a string!"
+            print attr, val
+            continue
         # skip non-public methods
-        if attr.startswith('_'): continue
+        if attr.startswith('_'):
+            continue
         # skip custom Nevow methods
-        if attr.startswith('child_'): continue
-        if attr.startswith('render_'): continue
-        if attr.startswith('data_'): continue
+        if attr.startswith('child_'):
+            continue
+        if attr.startswith('render_'):
+            continue
+        if attr.startswith('data_'):
+            continue
         # skip nevow.rend.Page methods
         if attr in rendPageAttrs:
             continue
-        obj = getattr(renderer, attr)
-        if callable(obj):
-            val = obj()
-        data[attr] = val
+        member = getattr(obj, attr)
+        if callable(member):
+            val = member()
+        print attr, val
+        try:
+            data[attr] = val
+        except Exception, err:
+            import pdb;pdb.set_trace()
     return data
+
+def getGenshiString(template, data):
+    tmpl = MarkupTemplate(template)
+    return tmpl.generate(**data).render('xhtml')
 
 class genshistr(xmlstr):
 
@@ -46,15 +64,12 @@ class genshistr(xmlstr):
         syntax in the template(s). If you will only be using Genshi, you can
         enable your own caching mechanism and not up-call to the parent class.
         """
-        data = {}
         renderer = inevow.IRenderer(ctx)
         data = getObjectData(renderer)
         # save a copy of the original, unmolested template, complete with
         # Genshi markup
         orig = self.template
-        tmpl = MarkupTemplate(self.template)
-        xmlStr = tmpl.generate(**data).render('xhtml')
-        self.template = xmlStr
+        self.template = getGenshiString(self.template, data)
         nevowRun = super(genshistr, self).load(ctx=ctx,
             preprocessors=preprocessors)
         # null out Nevow's caching of the template
@@ -64,24 +79,23 @@ class genshistr(xmlstr):
         self.template = orig
         return nevowRun
 
-class genshifile(xmlfile):
+class genshifile(xmlstr):
 
-    def _reallyLoad(self, path, ctx, preprocessors):
+    def __init__(self, filename):
+        self._filename = filename
 
+    def load(self, ctx=None, preprocessors=()):
+        if not hasattr(self, 'orig'):
+            self.orig = open(self._filename).read()
+            #self._reallyLoad(self._filename, ctx, preprocessors)
         renderer = inevow.IRenderer(ctx)
         data = getObjectData(renderer)
-        absPath = os.path.abspath(self._filename)
-        templateDir = os.path.dirname(absPath)
-        loader = TemplateLoader([templateDir])
-        orig = self.template
-        tmpl = loader.load(absPath)
-        xmlStr = tmpl.generate(**data).render('xhtml')
-        fh = StringIO(xmlStr)
-        doc = flat.flatsax.parse(fh, self.ignoreDocType,
-            self.ignoreComment)
-        for proc in preprocessors:
-            doc = proc(doc)
-        doc = flat.precompile(doc, ctx)
-        if self.pattern is not None:
-            doc = inevow.IQ(doc).onePattern(self.pattern)
-        return doc
+        self.template = getGenshiString(self.orig, data)
+        nevowRun = super(genshifile, self).load(ctx=ctx,
+            preprocessors=preprocessors)
+        # null out Nevow's caching of the template
+        self._cache = None
+        # revert to the unparsed Genshi template so that genshi attrs/methods
+        # have the chance to generate fresh data
+        self.template = self.orig
+        return nevowRun
