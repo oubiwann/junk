db38800dcb335fc37fd6afc93371a276b04c5e4f
Duncan McGreggor <oubiwann@lorien>
2009-06-22 00:14:47 -0500

    * Added missing else call in getChild, passing unexpected paths up to the   parent class. * Added code to the endpoint module. * Updated the webstore to use the endpoint. * Copied the spec file into the example directory.

---
+++ messaging/amqp/twisted/simple-web-store/01-orders/endpoint.py
@@ -0,0 +1,29 @@
+from twisted.internet.defer import inlineCallbacks, returnValue                                     
+
+from txamqp.content import Content
+
+import common
+
+
+def createOrderMessage(data, source):
+    message = Content("test data")
+    message["source"] = source
+    message["delivery mode"] = common.NON_PERSISTENT
+    return message
+
+
+@inlineCallbacks
+def sendMessage(request):
+    # create the message from the shopping cart request
+    message = createOrderMessage(request.args, "web store")
+    # connect to messaging server and get channel
+    producer = common.createProducer()
+    #import pdb;pdb.set_trace()
+    connection = yield common.getConnection(producer)
+    channel = yield common.getChannel(connection)
+    # send the message to the message queue
+    yield channel.basic_publish(
+        exchange=common.EXCHANGE_NAME, content=message,
+        routing_key=common.ROUTING_KEY)
+    # close the connections
+    yield common.cleanUp(connection, channel)
--- messaging/amqp/twisted/simple-web-store/01-orders/webstore.py
+++ messaging/amqp/twisted/simple-web-store/01-orders/webstore.py
@@ -1,9 +1,14 @@
-from twisted.web import server, resource
+from twisted.web import server
+from twisted.web.resource import Resource
 from twisted.application import service, internet
+
+import endpoint
+
 
 FORM = """
 <html>
 <body>
+<p><strong>Shopping Cart</stong></p>
 <form action="/processOrder" method="put">
 <input type="text" name="item" value="kilt"><br/>
 <input type="text" name="size" value="large"<br/>
@@ -22,36 +27,50 @@ THANKS = """
 </html>
 """
 
+OOPS = """
+<html>
+<body>
+<p><strong>Oops!</strong></p>
+<p>There was a problem placing your order.</p>
+</body>
+</html>
+"""
 
-class ShoppingCart(resource.Resource):
+
+class ShoppingCart(Resource):
     isLeaf = True
-    allowedMethods = ("GET",)
     def render_GET(self, request):
         return FORM
 
 
-class ProcessOrder(resource.Resource):
+class ProcessOrder(Resource):
     isLeaf = True
-    allowedMethods = ("GET",)
-    def render_GET(self, request):
-        item = request.args["item"][0]
-        size = request.args["size"][0]
+    def render(self, request):
         # code for saving these to a database
+        databaseCalls = "dummy"
         # code that creates a new order message
-        return THANKS
+        endpoint.sendMessage(request)
+        try:
+            request.write(THANKS)
+        except Exception, error:
+            request.write(OOPS)
+            request.write(error)
+        finally:
+            request.finish()
 
 
-class Store(resource.Resource):
-    allowedMethods = ("GET",)
+class Store(Resource):
     def getChild(self, path, request):
         if path == "shoppingCart":
             return ShoppingCart()
         elif path == "processOrder":
             return ProcessOrder()
+        else:
+            return Resource.getChild(self, path, request)
 
 
 application = service.Application("Web Store")
 storeService = service.IService(application)
 storeSite = server.Site(Store())
-server = internet.TCPServer(8080, storeSite)
-server.setServiceParent(storeService)
+storeServer = internet.TCPServer(8080, storeSite)
+storeServer.setServiceParent(storeService)
