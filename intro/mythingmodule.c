395813d91bfb982d78933cc07608340a47cd948d
Duncan McGreggor <duncan@ubuntu.com>
2010-11-07 00:17:57 -0600

    Added code from xxmodule example in Python source tree.

--- intro/mythingmodule.c
+++ intro/mythingmodule.c
@@ -2,9 +2,214 @@
 #include "mything.h"
 
 
-PyMethodDef methods[] = {
+typedef struct {
+    PyObject_HEAD
+    PyObject            *x_attr;        /* Attributes dictionary */
+} MyThingObject;
+
+static PyTypeObject MyThing_Type;
+
+#define MyThingObject_Check(v)      (Py_TYPE(v) == &MyThing_Type)
+
+/* MyThing methods */
+static void MyThing_dealloc(MyThingObject *self) {
+    Py_XDECREF(self->x_attr);
+    PyObject_Del(self);
+}
+
+static PyObject * MyThing_get_thing_one(MyThingObject *self, PyObject *args) {
+    int result = get_thing_one();
+    return Py_BuildValue("i", result);
+}
+
+//PyObject * MyThing_get_thing_two(PyObject *self) {
+//    int result = get_thing_two();
+//    return Py_BuildValue("i", result);
+//}
+
+PyMethodDef MyThing_methods[] = {
+    {"get_thing_one", (PyCFunction)MyThing_get_thing_one, METH_VARARGS,
+     PyDoc_STR("get_thing_one() -> 1")},
+    //{"get_thing_two", (PyCFunction)MyThing_get_thing_two, METH_VARARGS},
     {NULL, NULL},
 };
+
+static PyObject * MyThing_getattr(MyThingObject *self, char *name) {
+    if (self->x_attr != NULL) {
+        PyObject *v = PyDict_GetItemString(self->x_attr, name);
+        if (v != NULL) {
+            Py_INCREF(v);
+            return v;
+        }
+    }
+    return Py_FindMethod(MyThing_methods, (PyObject *)self, name);
+}
+
+static int MyThing_setattr(MyThingObject *self, char *name, PyObject *v) {
+    if (self->x_attr == NULL) {
+        self->x_attr = PyDict_New();
+        if (self->x_attr == NULL)
+            return -1;
+    }
+    if (v == NULL) {
+        int rv = PyDict_DelItemString(self->x_attr, name);
+        if (rv < 0)
+            PyErr_SetString(PyExc_AttributeError,
+                "delete non-existing MyThing attribute");
+        return rv;
+    }
+    else
+        return PyDict_SetItemString(self->x_attr, name, v);
+}
+
+static PyTypeObject MyThing_Type = {
+    /* The ob_type field must be initialized in the module init function
+     * to be portable to Windows without using C++. */
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "xxmodule.MyThing",             /*tp_name*/
+    sizeof(MyThingObject),          /*tp_basicsize*/
+    0,                              /*tp_itemsize*/
+    /* methods */
+    (destructor)MyThing_dealloc,    /*tp_dealloc*/
+    0,                              /*tp_print*/
+    (getattrfunc)MyThing_getattr,   /*tp_getattr*/
+    (setattrfunc)MyThing_setattr,   /*tp_setattr*/
+    0,                              /*tp_compare*/
+    0,                              /*tp_repr*/
+    0,                              /*tp_as_number*/
+    0,                              /*tp_as_sequence*/
+    0,                              /*tp_as_mapping*/
+    0,                              /*tp_hash*/
+    0,                              /*tp_call*/
+    0,                              /*tp_str*/
+    0,                              /*tp_getattro*/
+    0,                              /*tp_setattro*/
+    0,                              /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT,             /*tp_flags*/
+    0,                              /*tp_doc*/
+    0,                              /*tp_traverse*/
+    0,                              /*tp_clear*/
+    0,                              /*tp_richcompare*/
+    0,                              /*tp_weaklistoffset*/
+    0,                              /*tp_iter*/
+    0,                              /*tp_iternext*/
+    0,                              /*tp_methods*/
+    0,                              /*tp_members*/
+    0,                              /*tp_getset*/
+    0,                              /*tp_base*/
+    0,                              /*tp_dict*/
+    0,                              /*tp_descr_get*/
+    0,                              /*tp_descr_set*/
+    0,                              /*tp_dictoffset*/
+    0,                              /*tp_init*/
+    0,                              /*tp_alloc*/
+    0,                              /*tp_new*/
+    0,                              /*tp_free*/
+    0,                              /*tp_is_gc*/
+};
+/* --------------------------------------------------------------------- */
+
+static PyTypeObject Str_Type = {
+    /* The ob_type field must be initialized in the module init function
+     * to be portable to Windows without using C++. */
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "xxmodule.Str",             /*tp_name*/
+    0,                          /*tp_basicsize*/
+    0,                          /*tp_itemsize*/
+    /* methods */
+    0,                          /*tp_dealloc*/
+    0,                          /*tp_print*/
+    0,                          /*tp_getattr*/
+    0,                          /*tp_setattr*/
+    0,                          /*tp_compare*/
+    0,                          /*tp_repr*/
+    0,                          /*tp_as_number*/
+    0,                          /*tp_as_sequence*/
+    0,                          /*tp_as_mapping*/
+    0,                          /*tp_hash*/
+    0,                          /*tp_call*/
+    0,                          /*tp_str*/
+    0,                          /*tp_getattro*/
+    0,                          /*tp_setattro*/
+    0,                          /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
+    0,                          /*tp_doc*/
+    0,                          /*tp_traverse*/
+    0,                          /*tp_clear*/
+    0,                          /*tp_richcompare*/
+    0,                          /*tp_weaklistoffset*/
+    0,                          /*tp_iter*/
+    0,                          /*tp_iternext*/
+    0,                          /*tp_methods*/
+    0,                          /*tp_members*/
+    0,                          /*tp_getset*/
+    0, /* see initxx */         /*tp_base*/
+    0,                          /*tp_dict*/
+    0,                          /*tp_descr_get*/
+    0,                          /*tp_descr_set*/
+    0,                          /*tp_dictoffset*/
+    0,                          /*tp_init*/
+    0,                          /*tp_alloc*/
+    0,                          /*tp_new*/
+    0,                          /*tp_free*/
+    0,                          /*tp_is_gc*/
+};
+
+/* ---------- */
+
+static PyObject * null_richcompare(PyObject *self, PyObject *other, int op) {
+    Py_INCREF(Py_NotImplemented);
+    return Py_NotImplemented;
+}
+
+static PyTypeObject Null_Type = {
+    /* The ob_type field must be initialized in the module init function
+     * to be portable to Windows without using C++. */
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "xxmodule.Null",            /*tp_name*/
+    0,                          /*tp_basicsize*/
+    0,                          /*tp_itemsize*/
+    /* methods */
+    0,                          /*tp_dealloc*/
+    0,                          /*tp_print*/
+    0,                          /*tp_getattr*/
+    0,                          /*tp_setattr*/
+    0,                          /*tp_compare*/
+    0,                          /*tp_repr*/
+    0,                          /*tp_as_number*/
+    0,                          /*tp_as_sequence*/
+    0,                          /*tp_as_mapping*/
+    0,                          /*tp_hash*/
+    0,                          /*tp_call*/
+    0,                          /*tp_str*/
+    0,                          /*tp_getattro*/
+    0,                          /*tp_setattro*/
+    0,                          /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
+    0,                          /*tp_doc*/
+    0,                          /*tp_traverse*/
+    0,                          /*tp_clear*/
+    null_richcompare,           /*tp_richcompare*/
+    0,                          /*tp_weaklistoffset*/
+    0,                          /*tp_iter*/
+    0,                          /*tp_iternext*/
+    0,                          /*tp_methods*/
+    0,                          /*tp_members*/
+    0,                          /*tp_getset*/
+    0, /* see initxx */         /*tp_base*/
+    0,                          /*tp_dict*/
+    0,                          /*tp_descr_get*/
+    0,                          /*tp_descr_set*/
+    0,                          /*tp_dictoffset*/
+    0,                          /*tp_init*/
+    0,                          /*tp_alloc*/
+    0, /* see initxx */         /*tp_new*/
+    0,                          /*tp_free*/
+    0,                          /*tp_is_gc*/
+};
+
+
+/* ---------- */
 
 struct _IntConstantPair {
     char *constant_name;
@@ -20,11 +225,22 @@ static IntConstantPair _constants[] = {
 };
 
 PyMODINIT_FUNC
-initmything(void)
-{
+initmything(void) {
     PyObject *module, *dict, *tmp_obj;
-    module = Py_InitModule("mything", methods);
+    module = Py_InitModule("mything", MyThing_methods);
     dict = PyModule_GetDict(module);
+
+    /* Due to cross platform compiler issues the slots must be filled
+     * here. It's required for portability to Windows without requiring
+     * C++. */
+    Null_Type.tp_base = &PyBaseObject_Type;
+    Null_Type.tp_new = PyType_GenericNew;
+    Str_Type.tp_base = &PyUnicode_Type;
+
+    /* Finalize the type object including setting type of the new type
+     * object; doing it here is required for portability, too. */
+    if (PyType_Ready(&MyThing_Type) < 0)
+        return;
 
     /* Set integer constants */
     int i;
