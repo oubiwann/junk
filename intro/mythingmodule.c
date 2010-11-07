1e3513aae3c6418d30e49c37fd6fb70f543edea4
Duncan McGreggor <duncan@ubuntu.com>
2010-11-06 21:57:41 -0600

    Fixed tmp_obj type.

--- intro/mythingmodule.c
+++ intro/mythingmodule.c
@@ -22,15 +22,14 @@ static IntConstantPair _constants[] = {
 PyMODINIT_FUNC
 initmything(void)
 {
-    PyObject *module, *dict;
+    PyObject *module, *dict, *tmp_obj;
     module = Py_InitModule("mything", methods);
     dict = PyModule_GetDict(module);
 
     /* Set integer constants */
     int i;
-    int tmp_obj;
     for (i = 0; _constants[i].constant_name != 0; i++) {
-        tmp_obj = PyInt_FromLong(_constants[i].constant_value);
+        tmp_obj = Py_BuildValue("i", _constants[i].constant_value);
         PyDict_SetItemString(dict, _constants[i].constant_name, tmp_obj);
         Py_DECREF(tmp_obj);
     }
