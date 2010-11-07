f5457f2b3c7ee50006411ca41747ab7ae56b030d
Duncan McGreggor <duncan@ubuntu.com>
2010-11-06 21:36:41 -0600

    * Added a final (null) entry to struct so that for loop check will work. * Used full names for variables.

--- intro/mythingmodule.c
+++ intro/mythingmodule.c
@@ -15,21 +15,22 @@ typedef struct _IntConstantPair IntConstantPair;
 
 static IntConstantPair _constants[] = {
     {"THING_ONE", THING_ONE},
-    {"THING_TWO", THING_TWO}
+    {"THING_TWO", THING_TWO},
+    {(char*)NULL, 0}
 };
 
 void initmything()
 {
-    PyObject *m, *d;
-    m = Py_InitModule("mything", methods);
-    d = PyModule_GetDict(m);
+    PyObject *module, *dict;
+    module = Py_InitModule("mything", methods);
+    dict = PyModule_GetDict(module);
 
     /* Set integer constants */
     int i;
     int tmp_obj;
     for (i = 0; _constants[i].constant_name != 0; i++) {
         tmp_obj = PyInt_FromLong(_constants[i].constant_value);
-        PyDict_SetItemString(d, _constants[i].constant_name, tmp_obj);
+        PyDict_SetItemString(dict, _constants[i].constant_name, tmp_obj);
         Py_DECREF(tmp_obj);
     }
 }
