820080df6f283b05553737d53243abb8b4775030
Duncan McGreggor <duncan@ubuntu.com>
2010-11-06 22:09:33 -0600

    Updated C file main to return 0.

--- intro/mything.c
+++ intro/mything.c
@@ -12,11 +12,12 @@ int get_thing_two()
     return THING_TWO;
 }
 
-void main()
+int main()
 {
     int thing1;
     int thing2;
     thing1 = get_thing_one();
     thing2 = get_thing_two();
     printf("\nThing One: %d\nThing Two: %d\n", thing1, thing2);
+    return 0;
 }
