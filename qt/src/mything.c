a717776186d785010ac430d2cd347a4866bef64a
Duncan McGreggor <duncan@canonical.com>
2010-11-09 20:56:16 -0700

    Added Qt files for an example of the Qt bindings.

---
+++ qt/src/mything.c
@@ -0,0 +1,27 @@
+/* This file implements a very simple API for demonstration purposes in writing
+ * C extensions for Python.
+ */
+
+#include<stdio.h>
+#include "mything.h"
+
+
+int get_thing_one(void)
+{
+    return THING_ONE;
+}
+
+int get_thing_two(void)
+{
+    return THING_TWO;
+}
+
+int main(void)
+{
+    int thing1;
+    int thing2;
+    thing1 = get_thing_one();
+    thing2 = get_thing_two();
+    printf("\nThing One: %d\nThing Two: %d\n", thing1, thing2);
+    return 0;
+}
---
+++ qt/src/mything.h
@@ -0,0 +1,15 @@
+/* This file defines some constants and prototypes for the demonstration C
+ * code.
+ */
+
+#ifndef MYTHING_HEADER
+#define MYTHING_HEADER
+
+#define THING_ONE 1
+#define THING_TWO 2
+
+// function prototypes
+int get_thing_one(void);
+int get_thing_two(void);
+
+#endif
